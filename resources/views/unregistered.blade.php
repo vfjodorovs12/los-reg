1240a2ee8520d37b812cf617e2a14f3a2af961a2
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-04-22 22:03:55 +0100

    Create unregistered.blade.php
    @extends('web::layouts.grids.12')
    
    @section('title', 'Без регистрации')
    
    @section('page_header', 'Без регистрации')
    
    @section('full')
        <div class="card">
            <div class="card-header">
                <h3>Пользователи без регистрации</h3>
            </div>
            <div class="card-body">
                @if (count($unregisteredMembers) > 0)
                    <ul>
                        @foreach ($unregisteredMembers as $member)
                            <li>{{ $member }}</li>
                        @endforeach
                    </ul>
                @else
                    <p>Все пользователи зарегистрированы!</p>
                @endif
            </div>
        </div>
    @endsection

---
+++ resources/views/unregistered.blade.php
@@ -0,0 +1,24 @@
+@extends('web::layouts.grids.12')
+
+@section('title', 'Без регистрации')
+
+@section('page_header', 'Без регистрации')
+
+@section('full')
+    <div class="card">
+        <div class="card-header">
+            <h3>Пользователи без регистрации</h3>
+        </div>
+        <div class="card-body">
+            @if (count($unregisteredMembers) > 0)
+                <ul>
+                    @foreach ($unregisteredMembers as $member)
+                        <li>{{ $member }}</li>
+                    @endforeach
+                </ul>
+            @else
+                <p>Все пользователи зарегистрированы!</p>
+            @endif
+        </div>
+    </div>
+@endsection
