b4caad32b552749a0055d4434daa23ae71a4014e
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-18 23:04:11 +0100

    Update unregistered.blade.php
    @extends('web::layouts.grids.12')
    
    @section('title', 'Лос Рег')
    
    @section('page_header', 'Лос Рег')
    
    @section('full')
        <div class="panel panel-default">
            <div class="panel-heading">Добро пожаловать в модуль Лос Рег!</div>
            <div class="panel-body">
                Здесь можно реализовать ваш функционал.
            </div>
        </div>
    @endsection

--- resources/views/unregistered.blade.php
+++ resources/views/unregistered.blade.php
@@ -1,12 +1,14 @@
-@extends('web::layouts.app')
+@extends('web::layouts.grids.12')
 
-@section('page_title', 'Unregistered Characters')
-@section('page_header', 'Unregistered Characters')
+@section('title', 'Лос Рег')
 
-@section('content')
-    <div class="card">
-        <div class="card-body">
-            <p>Если ты видишь эту страницу — модуль los-reg работает!</p>
+@section('page_header', 'Лос Рег')
+
+@section('full')
+    <div class="panel panel-default">
+        <div class="panel-heading">Добро пожаловать в модуль Лос Рег!</div>
+        <div class="panel-body">
+            Здесь можно реализовать ваш функционал.
         </div>
     </div>
 @endsection
