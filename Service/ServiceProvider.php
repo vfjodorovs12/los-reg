2c533cf64c72931532a3142c2eed90ab22275eeb
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-18 17:55:29 +0100

    Update ServiceProvider.php
    <?php
    
    namespace Modules\LosReg;
    
    use Illuminate\Support\ServiceProvider;
    
    class LosRegServiceProvider extends ServiceProvider
    {
        /**
         * Bootstrap any application services.
         *
         * @return void
         */
        public function boot()
        {
            // Загрузка маршрутов
            $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
    
            // Загрузка представлений
            $this->loadViewsFrom(__DIR__ . '/resources/views', 'LosReg');
    
            // Публикация конфигураций
            $this->publishes([
                __DIR__ . '/config/config.php' => config_path('losreg.php'),
            ], 'config');
    
            // Загрузка миграций
            $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
        }
    
        /**
         * Register any application services.
         *
         * @return void
         */
        public function register()
        {
            // Регистрация конфигурации
            $this->mergeConfigFrom(
                __DIR__ . '/config/config.php', 'losreg'
            );
        }
    }

--- Service/ServiceProvider.php
+++ Service/ServiceProvider.php
@@ -1,17 +1,43 @@
+<?php
+
 namespace Modules\LosReg;
 
 use Illuminate\Support\ServiceProvider;
 
 class LosRegServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
     public function boot()
     {
+        // Загрузка маршрутов
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+
+        // Загрузка представлений
         $this->loadViewsFrom(__DIR__ . '/resources/views', 'LosReg');
+
+        // Публикация конфигураций
+        $this->publishes([
+            __DIR__ . '/config/config.php' => config_path('losreg.php'),
+        ], 'config');
+
+        // Загрузка миграций
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
     }
 
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
     public function register()
     {
-        //
+        // Регистрация конфигурации
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/config.php', 'losreg'
+        );
     }
 }
