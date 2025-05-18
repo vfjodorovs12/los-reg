27e8b4abd2ba79badeee62e393e0bcc83f275c63
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-18 20:58:18 +0100

    Update web.php
    <?php
    
    use Illuminate\Support\Facades\Route;
    use Vfjodorovs12\LosReg\LosRegController;
    
    Route::group(['middleware' => ['web', 'auth']], function () {
        Route::get('/losreg/unregistered', [LosRegController::class, 'showUnregistered'])->name('losreg.unregistered');
    });

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Vfjodorovs12\LosReg\LosRegController;
 
-// Группировка маршрутов под middleware SEAT (если нужно)
 Route::group(['middleware' => ['web', 'auth']], function () {
-    // Пример маршрута для списка незарегистрированных персонажей
     Route::get('/losreg/unregistered', [LosRegController::class, 'showUnregistered'])->name('losreg.unregistered');
-    // Добавь свои маршруты ниже
-    // Route::get('/losreg/что_угодно', [LosRegController::class, 'yourMethod']);
 });
