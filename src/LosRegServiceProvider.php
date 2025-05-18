d07c162421403b1e7ba4c55ccca47777d17bf524
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-18 23:02:16 +0100

    Update LosRegServiceProvider.php
    <?php
    
    namespace Vfjodorovs12\LosReg;
    
    use Seat\Services\AbstractSeatPlugin;
    
    class LosRegServiceProvider extends AbstractSeatPlugin
    {
        public function getMenu()
        {
            return [
                'name' => 'Лос Рег',
                'icon' => 'fa fa-magic',
                'route' => 'los-reg.index', // Имя роутера из routes/web.php
            ];
        }
    
        public function register()
        {
            // Можно регистрировать сервисы, если нужно
        }
    
        public function boot()
        {
            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
            $this->loadViewsFrom(__DIR__.'/../resources/views', 'los-reg');
        }
    }

--- src/LosRegServiceProvider.php
+++ src/LosRegServiceProvider.php
@@ -6,31 +6,23 @@ use Seat\Services\AbstractSeatPlugin;
 
 class LosRegServiceProvider extends AbstractSeatPlugin
 {
+    public function getMenu()
+    {
+        return [
+            'name' => 'Лос Рег',
+            'icon' => 'fa fa-magic',
+            'route' => 'los-reg.index', // Имя роутера из routes/web.php
+        ];
+    }
+
+    public function register()
+    {
+        // Можно регистрировать сервисы, если нужно
+    }
+
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'losreg');
-    }
-
-    public function register() {}
-
-    public function getName(): string
-    {
-        return 'los-reg';
-    }
-
-    public function getPackageRepositoryUrl(): string
-    {
-        return 'https://github.com/vfjodorovs12/los-reg';
-    }
-
-    public function getPackagistPackageName(): string
-    {
-        return 'los-reg';
-    }
-
-    public function getPackagistVendorName(): string
-    {
-        return 'vfjodorovs12';
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'los-reg');
     }
 }
