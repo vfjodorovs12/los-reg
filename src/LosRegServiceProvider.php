dca51d5291f462aa42e1936f0a0ccb48e2204af2
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-18 20:20:42 +0100

    Update LosRegServiceProvider.php
    <?php
    
    namespace Vfjodorovs12\LosReg;
    
    use Illuminate\Support\ServiceProvider;
    
    class LosRegServiceProvider extends ServiceProvider
    {
        /**
         * Выполняется после регистрации всех сервис-провайдеров.
         */
        public function boot()
        {
            // Подключение маршрутов
            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
    
            // Подключение шаблонов
            $this->loadViewsFrom(__DIR__.'/../resources/views', 'losreg');
        }
    
        /**
         * Регистрация сервисов и биндингов.
         */
        public function register()
        {
            // Оставь пустым, если ничего не нужно
        }
    }

--- src/LosRegServiceProvider.php
+++ src/LosRegServiceProvider.php
@@ -11,16 +11,11 @@ class LosRegServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Подключение маршрутов, если есть
-        // $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        // Подключение маршрутов
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
-        // Подключение миграций, если есть
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-
-        // Публикация файлов конфига, если есть
-        // $this->publishes([
-        //     __DIR__.'/../config/losreg.php' => config_path('losreg.php'),
-        // ], 'config');
+        // Подключение шаблонов
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'losreg');
     }
 
     /**
@@ -28,9 +23,6 @@ class LosRegServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Пример биндинга (если надо что-то регистрировать в контейнер)
-        // $this->app->singleton(SomeService::class, function ($app) {
-        //     return new SomeService();
-        // });
+        // Оставь пустым, если ничего не нужно
     }
 }
