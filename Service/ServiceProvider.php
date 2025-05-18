599201354ab229856f0dac3fe2a94a2c0bd1682d
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-18 17:53:02 +0100

    Update ServiceProvider.php
    namespace Modules\LosReg;
    
    use Illuminate\Support\ServiceProvider;
    
    class LosRegServiceProvider extends ServiceProvider
    {
        public function boot()
        {
            $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
            $this->loadViewsFrom(__DIR__ . '/resources/views', 'LosReg');
        }
    
        public function register()
        {
            //
        }
    }

--- Service/ServiceProvider.php
+++ Service/ServiceProvider.php
@@ -1,27 +1,15 @@
-<?php
-
 namespace Modules\LosReg;
 
 use Illuminate\Support\ServiceProvider;
 
-class ServiceProvider extends ServiceProvider
+class LosRegServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/resources/views', 'LosReg');
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
