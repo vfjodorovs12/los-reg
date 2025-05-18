ed6d4a02ec55f0f8f58951a2b95646da0fa3df9a
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-18 21:23:59 +0100

    Update unregistered.blade.php

--- resources/views/unregistered.blade.php
+++ resources/views/unregistered.blade.php
@@ -1,10 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Unregistered Characters</title>
-</head>
-<body>
-    <h1>Unregistered Characters</h1>
-    <p>Если ты видишь эту страницу — значит, плагин los-reg работает!</p>
-</body>
-</html>
+@extends('web::layouts.app')
+
+@section('page_title', 'Unregistered Characters')
+
+@section('page_header', 'Unregistered Characters')
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <p>Если ты видишь эту страницу — модуль los-reg работает!</p>
+        </div>
+    </div>
+@endsection
