b1c3e326a05a50661830b00fe81ff1cbaaa9dd05
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-20 07:04:20 +0100

    Update LosRegServiceProvider.php
    <?php
    
    namespace Vfjodorovs12\LosReg;
    
    use Seat\Services\AbstractSeatPlugin;
    
    class LosRegServiceProvider extends AbstractSeatPlugin
    {
        public function getName(): string
        {
            return 'Лос Рег';
        }
    
        public function getPackageRepositoryUrl(): string
        {
            return 'https://github.com/vfjodorovs12/los-reg';
        }
    
        public function getPackagistPackageName(): string
        {
            return 'los-reg';
        }
    
        public function getPackagistVendorName(): string
        {
            return 'vfjodorovs12';
        }
    
        public function register()
        {
            // Регистрируем меню
            $this->mergeConfigFrom(__DIR__ . '/Config/losreg.sidebar.php', 'package.sidebar');
        }
    
        public function boot()
        {
            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
            $this->loadViewsFrom(__DIR__.'/../resources/views', 'los-reg');
        }
    }

--- src/LosRegServiceProvider.php
+++ src/LosRegServiceProvider.php
@@ -28,20 +28,13 @@ class LosRegServiceProvider extends AbstractSeatPlugin
 
     public function register()
     {
-        //
+        // Регистрируем меню
+        $this->mergeConfigFrom(__DIR__ . '/Config/losreg.sidebar.php', 'package.sidebar');
     }
 
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'los-reg');
-
-        // Новый способ для меню в SeAT 5.x
-        if (function_exists('menu')) {
-            menu('sidebar')
-                ->group('Tools', function ($menu) {
-                    $menu->route('los-reg.index', 'Лос Рег', [], ['icon' => 'fa fa-magic']);
-                });
-        }
     }
 }
