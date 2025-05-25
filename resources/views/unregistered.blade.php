85187b9555b53ec3bd683b49538b6e96bbcb87de
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-25 19:42:36 +0100

    Update unregistered.blade.php
    @extends('web::layouts.grids.12')
    
    @section('title', 'Лос Рег')
    @section('page_header', 'Лос Рег')
    
    @section('full')
        <div class="panel panel-default">
            <div class="panel-heading">Незарегистрированные члены корпорации</div>
            <div class="panel-body">
                @if(empty($members))
                    <p>Все члены корпорации зарегистрированы!</p>
                @else
                    <table class="table table-striped">
                        <thead>
                            <tr>
                                <th>Character ID</th>
                                <th>Name</th>
                            </tr>
                        </thead>
                        <tbody>
                            @foreach($members as $member)
                                <tr>
                                    <td>{{ $member['character_id'] }}</td>
                                    <td>{{ $member['name'] }}</td>
                                </tr>
                            @endforeach
                        </tbody>
                    </table>
                @endif
            </div>
        </div>
    @endsection

--- resources/views/unregistered.blade.php
+++ resources/views/unregistered.blade.php
@@ -1,14 +1,32 @@
 @extends('web::layouts.grids.12')
 
 @section('title', 'Лос Рег')
-
 @section('page_header', 'Лос Рег')
 
 @section('full')
     <div class="panel panel-default">
-        <div class="panel-heading">Добро пожаловать в модуль Лос Рег!</div>
+        <div class="panel-heading">Незарегистрированные члены корпорации</div>
         <div class="panel-body">
-            Здесь можно реализовать ваш функционал.
+            @if(empty($members))
+                <p>Все члены корпорации зарегистрированы!</p>
+            @else
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Character ID</th>
+                            <th>Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($members as $member)
+                            <tr>
+                                <td>{{ $member['character_id'] }}</td>
+                                <td>{{ $member['name'] }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
         </div>
     </div>
 @endsection
