b3dd4da78875d2aca1cb000937692959c7da49e5
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-28 14:24:25 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Vfjodorovs12\LosReg\Http\Controllers\LosRegController;
 Route::middleware(['web', 'auth'])->prefix('los-reg')->name('los-reg.')->group(function () {
     Route::get('/', [LosRegController::class, 'index'])->name('index');
     Route::get('/unregistered', [LosRegController::class, 'unregistered'])->name('unregistered');
-    Route::get('/registered', [LosRegController::class, 'registered'])->name('registered');
+
 });
